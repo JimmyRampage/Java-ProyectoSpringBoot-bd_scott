fca5c2a20956bdbc12911ac4361999dc85bf854d
James Ovalle <jamesovallebarrera@gmail.com>
2026-02-11 16:33:42 +0100

    edit: subo nuevamente UserDetailsIServiceImpl

---
+++ src/main/java/com/bd_scott/app_bd_scott/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.bd_scott.app_bd_scott.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bd_scott.app_bd_scott.model.User;
+import com.bd_scott.app_bd_scott.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 1. Buscamos el usuario en nuestra DB
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        // 2. Convertimos nuestros ROLES a 'GrantedAuthority' de Spring Security
+        List<GrantedAuthority> autoridades = user.getRoles().stream()
+                .map(rol -> new SimpleGrantedAuthority(rol.getNombre()))
+                .collect(Collectors.toList());
+
+        // 3. Retornamos el objeto User de Spring con toda la info
+        return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            user.isEnabled(),
+            true,
+            true,
+            true,
+            autoridades
+        );
+    }
+}
